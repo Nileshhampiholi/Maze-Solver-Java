5be51fa5daa07be4a12017f62ed1bc8ad4a4ba82
nhamp <nhamp@DESKTOP-UTVEG4L>
2019-11-18 12:36:33 +0100

    Task 1 Lab 2

--- src/Lab2/Task1.java
+++ src/Lab2/Task1.java
@@ -1,9 +1,35 @@
 package Lab2;
 
+
+import lejos.hardware.lcd.LCD;
+import lejos.utility.Delay;
+
 public class Task1 {
 
 	public static void main(String[] args) {
-		// Start Here
+		print();
 	}
 
+
+
+public static void  print()  {
+	
+	int x =1;
+	int y =1;
+	String s = "Hello World";
+	int i = 0;
+	for (i=0; i<100; i++)
+	{
+		LCD.drawString(s, x, y);
+		Delay.msDelay(1000);
+		LCD.clear();
+		x= x+1;
+		if (x > 5)
+			{ 
+			 for (int j = 0; j<5; j++)
+				 {x= x-1;}
+			}
+	
+	}
+	}
 }
